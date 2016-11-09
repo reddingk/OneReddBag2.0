7abf941e034b3c283d207a45c281ff497b67ba05
Kris Redding <kredding@autoscribe.com>
2016-11-09 15:47:48 -0500

    update mobile design

--- app/js/controllers/headerCtrl.js
+++ app/js/controllers/headerCtrl.js
@@ -3,13 +3,17 @@
 
     angular.module('headerCtrl').controller('HeaderController', ['$state', function($state){
       var vm = this;
-      vm.checkActivePage = checkActivePage;      
+      vm.checkActivePage = checkActivePage;
 
       function checkActivePage(current) {
 			     var currentPage = $state;
            if (currentPage != null && currentPage.current.name.indexOf(current) > -1) { return true; }
 			     else { return false; }
 		  }
+      var navMain = $("#orb-inside-nav");
+      navMain.on("click", "a", null, function () {
+         navMain.collapse('hide');
+       });
 
     }]);
 
